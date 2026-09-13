1f94cdf9f844912d1ae5260271697fd872e8c697
Sagar Pal <munna44sagarpal@gmail.com>
2026-09-13 16:52:08 +0530

    Comment out Flask app initialization and main block

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+#app = Flask(__name__)
 # RMR CALCULATION FUNCTIONS
 
 #  PARAMETER 1: Strength of intact rock material 
@@ -407,5 +407,5 @@ def home():
     return render_template('index.html', result=result)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
